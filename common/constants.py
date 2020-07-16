7d86ac848e65b94c596480f2af7fd6fd0f69d7cf
ZhenyuGeng <freergzy8212@hotmail.com>
2020-07-16 16:27:38 +0200

    update

--- common/constants.py
+++ common/constants.py
@@ -1,7 +1,3 @@
-from config.settings import ALLOWED_HOSTS
-
-HOST_URL = 'http://'+ALLOWED_HOSTS[0]
-
 sys_template = {
-    'regist_email_content': '恭喜您注册成功， 请复制链接到浏览器上来激活邮箱 ：'+ HOST_URL +'/regist?activate_code=',
+    'regist_email_content': '恭喜您注册成功， 请复制激活码在主页激活邮箱 ：',
 }
